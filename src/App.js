299281dda31731aefdbfeef0a35f4b89ffe63573
Roudabeh Adnani <rodabeh.adnani@gmail.com>
2023-10-25 23:02:14 +0200

    initiate memory game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Styles from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={Styles.app}>
+      <h1>Magic Match</h1>
+      <button className={Styles.button}>
+        <h3>New Game</h3>
+      </button>
     </div>
   );
 }
